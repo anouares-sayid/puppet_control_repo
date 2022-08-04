4f2617125e32c6e4c2fc953e91f3cbfe37e52a09
Anouar Es-sayid <59542498+anouares-sayid@users.noreply.github.com>
2022-08-05 00:10:37 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,27 +1,28 @@
 class minecraft {
-      file {'/opt/minecraft/':
-          ensure => directory,
-          }
-      file {'/opt/minecraft/minecraft_server.jar':
-          ensure => file,
-          source => 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
-          before => Service['minecraft'],
-          }
-      package{'java':
-          ensure => present,
-          }
-      file {'/opt/minecraft/eula.txt':
-          ensure => file,
-          content => 'eula=true',
-          }
-      file {'/etc/systemd/system/minecraft.service':
-          ensure => file,
-          source => 'puppet:///modules/minecraft/minecraft.service',
-          }
-      service {'minecraft'
-            ensure => running,
-            enable => true,
-            require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
-      }
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+    before => Service[‘minecraft’],
+  }
 
+
+  package {‘java’:
+    ensure => present,
+  }
+  file {‘/opt/minecraft/eula.txt’:
+    ensure => file,
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
+    ensure => running,
+    enable => true,
+    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
+  }
 }
