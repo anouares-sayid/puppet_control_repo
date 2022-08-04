a688b1a08a2eb153e4612ea6569640c1eea6330c
Anouar Es-sayid <59542498+anouares-sayid@users.noreply.github.com>
2022-08-04 05:40:43 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNKIY2eGUXHwCO4KTFcjhcmRu096xgfTNVH4zaKiJzMOYB49QQAMzpXAK8vW8vhrHGsvFaOwpO33QeZw4QD+Cxh43Z1cmwxIwU/rDEjeZlKx7Dy7sgi76pYtu0NBcTez/rflEpKwtc9xVbn3jrO2XuiwZx/0Kk92ncOlAC6cemz+qjFl3bHLvPRIQfuIoZJyNY4TK0EE8bXpG0CRFFjZwNVF6R8dIJqim5pGifsmOCgilZ5F2mCSYCQRz0JdMiqQazWEDs8Bat+2FgHRfvmM7G+Xa0kGSFQaE4G5Bmv7QiFfy+Dwh4Wp/m3VaNsIrxovXpow39cOXUYjWZZZkiamjX',
 	}  
 }
