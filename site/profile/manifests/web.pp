ab542c9f4f1583c9de897eb15906ccbc06d53298
Anouar Es-sayid <59542498+anouares-sayid@users.noreply.github.com>
2022-08-04 05:31:23 +0100

    Update web.pp

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,8 +1,8 @@
 class profile::web{
     include nginx
-    host { 'app':
+    host { 'web':
      ensure       => 'present',
-    host_aliases => ['apphost'],
+    host_aliases => ['webhost'],
     ip           => '172.18.0.2',
      target       => '/etc/hosts',
         }
